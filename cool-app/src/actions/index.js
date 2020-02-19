142e27ad15273f6549cbd5709263d9e6821425f6
Alec Dye <alecdye@gmail.com>
2020-02-19 15:49:17 -0800

    adding proxy to get request

--- cool-app/src/actions/index.js
+++ cool-app/src/actions/index.js
@@ -5,7 +5,8 @@ export const FETCH_DATA = "FETCH_DATA";
 export const getData = () => dispatch => {
     dispatch({ type: FETCH_DATA });
     axios
-        .get('http://xkcd.com/info.0.json')
+        .get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json')
+        //data.props
         .then(res => {
             console.log(res);
             // dispatch({ type: UPDATE_COMIC, payload: res.data})
